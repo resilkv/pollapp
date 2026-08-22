73de71d47b61780bf75950b89bd0b34e73854037
resilkv <resilradhakrishnan@gmail.com>
2026-08-22 12:17:46 +0530

    changes in the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
-set -e #stop if anything fails
+set -e
 
 echo "Waiting for database..."
 
-until pg_isready -h "$DBHOST" -p "$DBPORT" -U "$DBUSER" -d "$DBNAME"; do
+until pg_isready \
+  -h "$DBHOST" \
+  -p "$DBPORT" \
+  -U "$DBUSER" \
+  -d "$DBNAME"; do
+
   echo "Postgres is unavailable - sleeping"
   sleep 1
+
 done
+
+echo "Postgres is ready!"
+
+echo "Running migrations..."
 
 python manage.py migrate
 
-echo "Collect Static"
+echo "Migrations completed!"
+
+echo "Collect Static..."
 
 python manage.py collectstatic --noinput
+
+echo "Starting application..."
 
 exec "$@"
