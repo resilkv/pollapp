a534b0d8bfe0c2b63f1917721f68dd1be55487ba
resilkv <resilradhakrishnan@gmail.com>
2026-08-22 12:01:55 +0530

    changes in the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e #stop if anything fails
 
 echo "Waiting for database..."
 
-while ! pg_isready -h localhost -p 5432; do
-    echo "Postgres is unavailable - sleeping"
-    sleep 1
+until pg_isready -h "$DBHOST" -p "$DBPORT" -U "$DBUSER" -d "$DBNAME"; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 1
 done
 
 python manage.py migrate
